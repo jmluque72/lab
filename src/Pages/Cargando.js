83fb610c284bbdecabd59e70381c761a4a72841c
Manuel Luque <manuelluque@Manuels-MBP.fibertel.com.ar>
2021-05-16 23:02:45 -0300

    news

--- src/Pages/Cargando.js
+++ src/Pages/Cargando.js
@@ -3,6 +3,7 @@ import {Grid} from '@material-ui/core';
 import './Main.css'
 import logoCargando from '../assets/logoCargando.png'
 import Background2 from '../assets/background_event_program.png'
+import BackgroundR from '../assets/background_responsive.jpg'
 
 
 class Cargando extends React.Component {
@@ -16,11 +17,20 @@ class Cargando extends React.Component {
 
     render() {
         const min = window.innerWidth >= 1000
-        return (
-            <div style={{height: window.innerHeight, width:'100%',backgroundImage:`url(${Background2})`,backgroundSize:'cover'}}>
-                <img src={logoCargando} width='auto' height='90%'></img>
-            </div>
-        );
+        if (min) {
+            return (
+                <div style={{height: window.innerHeight, width:'100%',backgroundImage:`url(${Background2})`,backgroundSize:'cover'}}>
+                    <img src={logoCargando} width='auto' height='90%'></img>
+                </div>
+            );
+        } else {
+            return (
+                <div style={{height: window.innerHeight, width:'100%'}}>
+                    <img src={BackgroundR} width='auto' width='100%'></img>
+                </div>
+            );
+        }
+        
     }
 }
 export default Cargando;
