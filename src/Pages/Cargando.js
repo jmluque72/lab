460c0a3b39a284199a77140240045b5ee54b00f7
Karin <102821746+karincaceres@users.noreply.github.com>
2022-06-21 13:06:21 -0300

    Add files via upload

--- src/Pages/Cargando.js
+++ src/Pages/Cargando.js
@@ -35,7 +35,7 @@ class Cargando extends React.Component {
                             <Grid item xs={12} sm={12} style={{ display:'flex',flexDirection: 'column',alignContent:'center' }}>
 
                                     <div  style={{display: 'flex',justifyContent:'center', alignContent:'center'}}>
-                                    <img style={{ maxWidth: minheight ? 700 : 400, marginTop: '10%', alignItems: 'center', }} src={LogoTop}></img>
+                                        <img style={{ maxWidth: minheight ? 700 : 400, marginTop: '10%', alignItems: 'center', }} src={LogoTop}></img>
 
                                     </div>
                                     <div  style={{ alignContent:'center',justifyContent:'center'}}>
@@ -60,9 +60,11 @@ class Cargando extends React.Component {
               <div>
                     <div style={{ display: 'flex',  height: '100%vh', width:'100%' }}>
                     <div style={{display:'flex',justifyContent:'center',width:'100%',backgroundImage:`url(${Background})`,backgroundSize:'cover', height:'100vh'}}>
-                            <Grid item xs={11} sm={11} md={8} style={{ alignItems: 'center' ,width: '100%', height: '100%',   marginTop: '10%', marginBottom: '0%', position: 'absolute' }}>
-                                 <img style={{ maxWidth: minWidth ? 400 : 300 , maxHeight: minheight ? 250 * 0.86 : 150 * 0.86, marginTop: '40%',alignItems: 'center' }} src={LogoTop}></img>
+                          <Grid item xs={12} sm={12} style={{ display:'flex',flexDirection: 'column',alignContent:'center' }}>
 
+                                    <div  style={{display: 'flex',justifyContent:'center', alignContent:'center'}}>
+                                        <img style={{ maxWidth: minWidth ? 400 : 300 , maxHeight: minheight ? 250 * 0.86 : 240 * 0.86, marginTop: '50%',alignItems: 'center' }} src={LogoTop}></img>
+                                    </div>
                                 <div style={{ display: 'flex',flexDirection: 'column',alignItems:'center'}}>
                                      <img style={{   maxWidth: minWidth ? 400 : 300, minWidth: 300,alignItems: 'center' }} src={logoCargando}></img>
                                 </div>
@@ -78,34 +80,6 @@ class Cargando extends React.Component {
     }
 
 
-
-
-            //return (
-            //    <div style={{ minWidth: 400, }}>
-            //        <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', backgroundImage: `url(${BackgroundCargando})`, backgroundSize: 'cover', height:height  }}>
-            //            <Grid item xs={11} sm={12} md={12} style={{ width: '100%',padding: '2%', marginTop: '2%', marginBottom: '2%', position: 'relative' }}>
-            //                <div >
-            //                <Grid item xs={12} sm={12} md={12} style={{ width: '100%', height: '100%',   padding: '2%', marginTop: '2%',  position: 'relative' }}>
-            //                        <img style={{ width: 480, height: 300 * 0.86, marginTop: '5%', padding: '2%', }} src={LogoTop}></img>
-            //                </Grid>
-            //                </div>
-            //                <div style={{ display: 'flex',  flexDirection: 'row', height: this.state.height }}>
-            //                    <Grid item xs={12} sm={8} container direction='row' style={{ justifyContent: 'center', alignItems: 'center', display: 'flex', flexDirection: 'row' }}>
-            //                        <img style={{ padding: '2%', maxWidth: 700, minWidth:500}} src={logoCargando}></img>
-            //                    </Grid>
-
-            //                </div>
-            //            </Grid>
-            //            <div>
-            //                 <img style={{ maxWidth: 200, bottom: '0%', position: 'absolute', alignItems: 'center' }} src={pieCargando}></img>
-            //                 <img style={{ width: 118 * 0.7, height: 84 * 0.7, position: 'absolute', bottom: '0%', right: '3%' }} src={logoNovo}></img>
-            //            </div>
-            //        </div>
-
-            //    </div>
-
-            //       )
-            //}
 
         }
 
