6a1ca818090136ce5b89c96baca3b89df57b038a
Karin <102821746+karincaceres@users.noreply.github.com>
2022-07-20 23:56:59 -0300

    Add files via upload

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,5 @@
 export const colors = {
-    gray : '#4B5E5E',
-    degrade_orange : 'linear-gradient(90deg, #E50051, #E50051)'
+    gray : '#4d2d7b',
+    degrade_orange : 'linear-gradient(70deg, #4d2d7b, #884EA0)'
 }
 
